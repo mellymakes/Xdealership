79e9733ce81101d6e47de0a0d5a1c31df1729b4e
happyprogrammer-code <reksbark699@gmail.com>
2021-05-11 07:29:21 +0700

    sec prod commit

---
+++ src/all/order/components/orderData.js
@@ -0,0 +1,78 @@
+import {redFerrariFace, blueFerrariFace, redFerrariSide, blueFerrariSide} from '../../../imgs'
+
+
+
+const orderData = [
+
+    {
+        id: 1,
+        carName: 'Ferrari 1213 ENG',
+        img: redFerrariSide,
+        price: '8.000.000',
+        madeInYear: 2006,
+        madeInCountry: 'Indonesia',
+        weight:{
+            num: 300,
+            unit: 'kg'
+        },
+        height: {
+            num: 2.5,
+            unit: 'M'
+        }
+        
+    },
+    {
+        id: 2,
+        carName: 'Ferrari 5134 ENG',
+        img: blueFerrariFace,
+        price: '20.000.000',
+        madeInYear: 2020,
+        madeInCountry: 'Indonesia',
+        weight:{
+            num: 300,
+            unit: 'kg'
+        },
+        height: {
+            num: 2.5,
+            unit: 'M'
+        }
+        
+    },
+    {
+        id: 3,
+        carName: 'Ferrari 4513 ENG',
+        img: redFerrariFace,
+        price: '18.000.000',
+        madeInYear: 2019,
+        madeInCountry: 'Indonesia',
+        weight:{
+            num: 300,
+            unit: 'kg'
+        },
+        height: {
+            num: 2.5,
+            unit: 'M'
+        }
+        
+    },
+    {
+        id: 4,
+        carName: 'Ferrari 6413 ENG',
+        img: blueFerrariSide,
+        price: '28.000.000',
+        madeInYear: 2021,
+        madeInCountry: 'Indonesia',
+        weight:{
+            num: 300,
+            unit: 'kg'
+        },
+        height: {
+            num: 2.5,
+            unit: 'M'
+        }
+        
+    },
+
+]
+
+export default orderData
